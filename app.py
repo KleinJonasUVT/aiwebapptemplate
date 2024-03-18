11af16a34d91409e60c88ab2b3d39a75a902c3db
Jonas Klein <jonasklein2001@gmail.com>
2024-03-18 23:40:39 +0100

    Add Flask app instance

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, jsonify, request, redirect, session, u
 from datetime import datetime, timedelta
 import secrets
 
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template('index.html')
